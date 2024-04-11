980897f00370d5c285d773965837dc1a59d52446
WTJ <ubuntu@ip-172-31-20-196.ec2.internal>
2024-04-11 14:31:27 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount:i32)-> i32 {
+    let mut price = 2;
+    if amount > 40 {
+        price = 1;
+    }
+    return price * amount;
+}
 
 // Don't modify this function!
 #[test]
