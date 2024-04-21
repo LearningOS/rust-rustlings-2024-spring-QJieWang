a20fc39fdeaa750da60e18e8bbc61661b63c3231
WTJ <ubuntu@ip-172-31-20-196.ec2.internal>
2024-04-21 17:06:21 +0000

    upgrade

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,7 +9,6 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -17,22 +16,30 @@ use std::num::ParseIntError;
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
-    ParseInt(ParseIntError),
+    ParseInt(ParseIntError)
 }
 
 impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+    fn from_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
-fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+fn parse_pos_nonzero(s: &str)
+    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
+{
+    match s.parse() {
+        Ok(n) => {
+            match PositiveNonzeroInteger::new(n) {
+                Ok(n) => Ok(n),
+                Err(err) => Err(ParsePosNonzeroError::from_creation(err)),
+            }
+        }
+        Err(err) => Err(ParsePosNonzeroError::from_parse_int(err)),
+    }
 }
 
 // Don't change anything below this line.
@@ -51,7 +58,7 @@ impl PositiveNonzeroInteger {
         match value {
             x if x < 0 => Err(CreationError::Negative),
             x if x == 0 => Err(CreationError::Zero),
-            x => Ok(PositiveNonzeroInteger(x as u64)),
+            x => Ok(PositiveNonzeroInteger(x as u64))
         }
     }
 }
